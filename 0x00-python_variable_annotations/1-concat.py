8715d76d4298d15828c6b258e50233f62953648f
Alfred Tuva <muindetuva@gmail.com>
2023-04-05 13:43:57 +0000

    Add task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+'''
+Module contains function that concatanates two strings
+'''
+
+def concat(str1: str, str2: str) -> str:
+    '''Joins two strings and returns a concated one.
+
+    Args:
+        str1 (str): First string.
+        str2 (str): Second string.
+
+    Returns:
+        str: The concatanated string.
+    '''
+    return str1 + str2
