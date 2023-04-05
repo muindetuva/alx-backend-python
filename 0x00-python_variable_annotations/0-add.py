3b95fb74c7bd912943a70ad707bbb7ceaed9ad26
Alfred Tuva <muindetuva@gmail.com>
2023-04-05 12:36:11 +0000

    Add task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Module contains a function that adds two numbers
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''Adds a two numbers and returns the sum.
+
+    Args:
+        a (int): First integer
+        b (int): Second integer
+
+    Returns:
+        int: The sum of the two numbers
+    '''
+
+    return a + b
