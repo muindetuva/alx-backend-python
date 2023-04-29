a5823d93f9bfeec9113255e59a1dc6994f8497f3
Alfred Tuva <muindetuva@gmail.com>
2023-04-29 20:44:20 +0000

    Change order of union args

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ Module contains function that returns a tuple
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''Function that takes a string and float or int and returns and tuple
 
     Args:
