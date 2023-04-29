92e4a50a91dd053276dd028fde26d05104f31252
Alfred Tuva <muindetuva@gmail.com>
2023-04-29 08:47:26 +0000

    Add task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+Module contains function that floors a float
+'''
+import math
+
+def floor(n: float) -> float:
+    '''Takes a float and returns the floor of the float
+
+    Args:
+        n (float): Float to be floored
+
+    Returns:
+        float: The floored float
+    '''
+    return math.floor(n)
