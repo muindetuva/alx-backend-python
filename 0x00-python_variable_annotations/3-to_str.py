32f290f7ab52cf542c3e3985e917a6cb420618da
Alfred Tuva <muindetuva@gmail.com>
2023-04-29 09:00:08 +0000

    Add task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Module contains function that converts a float to a string
+'''
+
+
+def to_str(n: float) -> str:
+    '''Takes a float and returns a string
+
+    Args:
+        n (float): Float to be converted
+
+    Returns:
+        str: String representation of the float
+    '''
+
+    return str(n)
