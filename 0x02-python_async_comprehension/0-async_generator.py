bd26257cf02c1050bd33b9bf8dd2ea7c7b952b54
Alfred Tuva <muindetuva@gmail.com>
2025-05-17 13:19:33 +0300

    update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,10 +9,10 @@ with a 1-second delay between each value.
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronously generate 10 random float values between 0 and 10.
 
